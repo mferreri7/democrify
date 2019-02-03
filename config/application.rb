1d4bbcddda2b3f32a3bf2889ba1872bede3285c0
Miguel Ferrer <miguef7@gmail.com>
2019-02-02 20:00:54 -0500

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Top20NovedadesColombia
+module Democrify
   class Application < Rails::Application
     config.generators do |generate|
           generate.assets false
