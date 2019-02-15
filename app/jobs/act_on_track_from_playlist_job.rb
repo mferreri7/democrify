d014a3f8000f287b94718c433cc5a28b789e554c
Miguel Ferrer <miguef7@gmail.com>
2019-02-15 10:14:02 -0500

    fix bug

--- app/jobs/act_on_track_from_playlist_job.rb
+++ app/jobs/act_on_track_from_playlist_job.rb
@@ -12,7 +12,7 @@ class ActOnTrackFromPlaylistJob < ApplicationJob
 
     user = User.find(user_id)
     @spotify_user = RSpotify::User.new(user.spotify_user_hash)
-    relocate_playlist = RSpotify::Playlist.find(user.spotify_id, relocate_playlist_id) if category == "Relocate"
+    relocate_playlist = RSpotify::Playlist.find(spotify_user_id, relocate_playlist_id) if category == "Relocate"
     playlist = RSpotify::Playlist.find(spotify_user_id, spotify_playlist_id)
     track = RSpotify::Track.find(spotify_track_id)
     relocate_playlist.add_tracks!([track]) if category == "Relocate"
